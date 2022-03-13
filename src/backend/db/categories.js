8f12d8e8edd92b6a6f3b62d81f2184af5bfb77a3
launchpad5682 <launchpad5682@gmail.com>
2022-03-13 18:40:32 +0530

    chore: fake data added

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -11,17 +11,27 @@ export const categories = [
     categoryName: "fiction",
     description:
       "literature in the form of prose, especially novels, that describes imaginary events and people",
+    path: "https://images.unsplash.com/photo-1474932430478-367dbb6832c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmljdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
+    categoryName: "history",
     description:
       "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    path: "https://images.unsplash.com/photo-1461360370896-922624d12aa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
   },
   {
     _id: uuid(),
-    categoryName: "horror",
+    categoryName: "political science",
     description:
       "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    path: "https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+  },
+  {
+    _id: uuid(),
+    categoryName: "philosophy",
+    description:
+      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    path: "https://images.unsplash.com/photo-1620662831351-9f68f76d0b9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
   },
 ];
--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -5,26 +5,142 @@ import { v4 as uuid } from "uuid";
  * You can add products of your wish with different attributes
  * */
 
+const baseURL = "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/public/calm-shop-books/"
+
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
-  },
-  {
-    _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
-  },
-  {
-    _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
+    title: "1984",
+    author: "George Orwell",
+    price: "300",
+    off: "26",
     categoryName: "fiction",
+    path: `${baseURL}1984.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "A short history of the World",
+    author: "Wells H.G.",
+    price: "299",
+    off: "55",
+    categoryName: "history",
+    path: `${baseURL}a-short-history-of-time.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "The Alchemist",
+    author: "Paulo Coehlo",
+    price: "248",
+    off: "25",
+    categoryName: "fiction",
+    path: `${baseURL}alchemist.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "Alexander: The Great",
+    author: "Abbott Jacob",
+    price: "150",
+    off: "40",
+    categoryName: "history",
+    path: `${baseURL}alexander.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "The Art of War",
+    author: "Tzu Sun",
+    price: "149",
+    off: "53",
+    categoryName: "history",
+    path: `${baseURL}art-of-war.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "Beyond Good and Evil",
+    author: "Nietzsche Friedrich",
+    price: "125",
+    off: "32",
+    categoryName: "philosophy",
+    path: `${baseURL}beyond-good-and-evil.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "Metamorphosis",
+    author: "Kafka Franz",
+    price: "177",
+    off: "61",
+    categoryName: "fiction",
+    path: `${baseURL}metamorphosis.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "Sapiens",
+    author: "Harari Yuval Noah",
+    price: "499",
+    off: "42",
+    categoryName: "history",
+    path: `${baseURL}sapiens.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "The Importance of Being Earnest",
+    author: "Wilde Oscar",
+    price: "75",
+    off: "30",
+    categoryName: "fiction",
+    path: `${baseURL}the-importance of being earnest.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "The Origin of Species",
+    author: "Charles Darwin",
+    price: "269",
+    off: "55",
+    categoryName: "history",
+    path: `${baseURL}the-origin-of-species.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "The Politics",
+    author: "Aristotle",
+    price: "199",
+    off: "47",
+    categoryName: "political science",
+    path: `${baseURL}the-politics.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "The Republic",
+    author: "Plato",
+    price: "199",
+    off: "47",
+    categoryName: "history",
+    path: `${baseURL}the-republic.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "War and Peace",
+    author: "Leo Tolstoy",
+    price: "499",
+    off: "26",
+    categoryName: "fiction",
+    path: `${baseURL}war-and-peace.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "Wealth of Nations",
+    author: "Adam Smith",
+    price: "399",
+    off: "49",
+    categoryName: "political science",
+    path: `${baseURL}wealth-of-nations.jpg`
+  },
+  {
+    _id: uuid(),
+    title: "Wuthering Heights",
+    author: "Bronte Emily",
+    price: "199",
+    off: "49",
+    categoryName: "fiction",
+    path: `${baseURL}wuthering-heights.jpg`
   },
 ];
