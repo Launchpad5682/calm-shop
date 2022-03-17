6bb80bd84d4f17b25ca080f0a2c6f66af9605a89
launchpad5682 <launchpad5682@gmail.com>
2022-03-17 17:45:50 +0530

    feat: Cart route added

--- src/routes/Cart/Cart.jsx
+++ src/routes/Cart/Cart.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+import CartPriceCard from "../../components/Cart/CartPriceCard/CartPriceCard";
+import { EmptyCart } from "../../components/Cart/EmptyCart/EmptyCart";
+import { ProductCartCard } from "../../components/Cart/ProductCartCard/ProductCartCard";
+import { useProvider } from "../../context/provider-context";
+import { getCartTotal } from "../../utils";
+import "./Cart.css";
 
 export function Cart() {
-    return (
-        <div>Cart</div>
-    )
+  const { cart } = useProvider();
+  const { products } = cart;
+
+  const cartTotal = getCartTotal(cart);
+  return (
+    <main className="cart__section padding--sm">
+      {products.length > 0 ? (
+        <>
+          <div className="wishlist--cards">
+            {products?.map((product) => (
+              <ProductCartCard key={product._id} product={product} />
+            ))}
+          </div>
+          <div>
+            <CartPriceCard cartTotal={cartTotal} />
+          </div>
+        </>
+      ) : (
+        <EmptyCart caption="EMPTY CART" />
+      )}
+    </main>
+  );
 }
