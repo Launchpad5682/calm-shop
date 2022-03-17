9201ee6bbc6a85237d6f1fe7823e14fb38b20121
launchpad5682 <launchpad5682@gmail.com>
2022-03-17 17:46:35 +0530

    feat: ProductListing route created

--- src/routes/ProductsListing/ProductsListing.jsx
+++ src/routes/ProductsListing/ProductsListing.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import "./ProductsListing.css";
+import React from "react";
+import { Filters } from "../../components/Filters/Filters";
+import { ProductCard } from "../../components/ProductCard/ProductCard";
+import { ProductsListingContainer } from "./ProductsListingContainer";
 
 export function ProductsListing() {
-    return (
-        <div>ProductsListing</div>
-    )
+  const { processedProducts } = ProductsListingContainer();
+
+  return (
+    <div className="main--section">
+      <Filters />
+      <main className="main--grid padding--sm">
+        <div className="subheading">
+          <span className="h5__typography typography--black bold--typography">
+            Products {processedProducts.length}
+          </span>
+          <button
+            className="button--sm button__nav button--red button__rounded--sm button__nav--black filter--btn"
+            id="open--filter"
+          >
+            <span className="button__typography typography--black">Filter</span>
+          </button>
+        </div>
+        {processedProducts.length > 0 ? (
+          <div className="grid-4-item padding--sm--vertical">
+            {processedProducts?.map((product) => (
+              <ProductCard product={product} key={product._id} />
+            ))}
+          </div>
+        ) : (
+          <div className="h6__typography">No products to show</div>
+        )}
+      </main>
+    </div>
+  );
 }
---
+++ src/routes/ProductsListing/ProductsListingContainer.js
@@ -0,0 +1,71 @@
+import { useProvider } from "../../context/provider-context";
+
+export function ProductsListingContainer() {
+  const { products, filters } = useProvider();
+  const { sortBy, priceUpperRange, filterRating, categories } = filters;
+
+  const sortProducts = (products) => {
+    // sort() uses in-place sorting
+    const temp = [...products];
+    if (sortBy === "LOW_TO_HIGH") {
+      return temp.sort(
+        (first, second) =>
+          Number(first.discountedPrice) - Number(second.discountedPrice)
+      );
+    } else if (sortBy === "HIGH_TO_LOW") {
+      return temp.sort(
+        (first, second) =>
+          Number(second.discountedPrice) - Number(first.discountedPrice)
+      );
+    }
+    return products;
+  };
+
+  const productsInRange = (products, range) => {
+    if (products?.length > 0 && range) {
+      return [
+        ...products.filter(
+          (product) => Number(product.discountedPrice) < Number(range)
+        ),
+      ];
+    }
+    return products;
+  };
+
+  const ratingFilter = (products, rating) => {
+    if (products?.length > 0 && rating) {
+      return [...products.filter((product) => Number(product.rating) > rating)];
+    }
+    return products;
+  };
+
+  const categoryFilter = (products, categories) => {
+    const categoryFilter = Object.values(categories).reduce(
+      (acc, curr) => acc || curr,
+      false
+    );
+    const set = Object.keys(categories).filter(
+      (key) => categories[key] === true
+    );
+    if (categoryFilter === true) {
+      const arr = [
+        ...products.filter((product) => set.includes(product.categoryName)),
+      ];
+      return arr;
+    }
+
+    return products;
+  };
+
+  const sortedProducts = sortProducts(products?.products);
+  const ratingFilteredProducts = ratingFilter(sortedProducts, filterRating);
+  const categoryFilteredProducts = categoryFilter(
+    ratingFilteredProducts,
+    categories
+  );
+  const processedProducts = productsInRange(
+    categoryFilteredProducts,
+    priceUpperRange
+  );
+  return { processedProducts };
+}
