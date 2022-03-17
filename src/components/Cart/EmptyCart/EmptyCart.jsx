4f9e3a6687423a70cb53b6b3667dda084b529ab6
launchpad5682 <launchpad5682@gmail.com>
2022-03-17 17:35:26 +0530

    feat: Cart Price Card, Empty Cart vector image, and Product Card for cart added

---
+++ src/components/Cart/EmptyCart/EmptyCart.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./EmptyCart.css";
+
+export function EmptyCart({ caption }) {
+  return (
+    <div className="full__page--vector">
+      <div className="vector--container">
+        <img
+          src="https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/public/calm-shop-books/cart.png"
+          className="image__fit"
+        />
+      </div>
+      <span className="h3__typography">{caption}</span>
+    </div>
+  );
+}
