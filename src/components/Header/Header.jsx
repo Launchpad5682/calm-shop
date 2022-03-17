e31d9e1d8fb45a6b55a6279d3c6664b939cf04bc
launchpad5682 <launchpad5682@gmail.com>
2022-03-17 22:33:17 +0530

    feat: Header Navigation component added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,50 @@
-import React from 'react'
-import "./Header.css"
-import { BsFillCartFill, BsFillHeartFill, BsFillPersonBadgeFill, BsSearch } from "react-icons/bs"
+import React from "react";
+import "./Header.css";
+import {
+  BsFillCartFill,
+  BsFillHeartFill,
+  BsFillPersonBadgeFill,
+  BsSearch,
+} from "react-icons/bs";
 
 export function Header() {
-    return (
-        <header class="header header--dark">
-            <span class="logo">
-                <img src="https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/public/calm-shop-books/calm-shop-logo.png" alt="" class="logo--img" />
-                <h6 class="h4__typography typography--white header__title">
-                    Calm Shop
-                </h6>
+  return (
+    <header className="header header--dark">
+      <span className="logo">
+        <img
+          src="https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/public/calm-shop-books/calm-shop-logo.png"
+          alt=""
+          className="logo--img"
+        />
+        <h6 className="h4__typography typography--white header__title">
+          Calm Shop
+        </h6>
+      </span>
+      <div className="search__container">
+        <input type="text" name="" id="" className="search__input" />
+        <span className="search__btn">
+          <BsSearch />
+        </span>
+      </div>
+      <nav className="">
+        <ul className="nav__list">
+          <button className="badge__icon badge__icon--blue">
+            <span className="badge__icon--counter icon__counter--pink">
+              100
             </span>
-            <div class="search__container">
-                <input type="text" name="" id="" class="search__input" />
-                <span class="search__btn"><BsSearch /></span>
-            </div>
-            <nav class="">
-                <ul class="nav__list">
-                    <button class="badge__icon badge__icon--blue">
-                        <span class="badge__icon--counter icon__counter--pink">100</span>
-                        <BsFillHeartFill />
-                    </button>
-                    <button class="badge__icon badge__icon--blue">
-                        <BsFillCartFill />
-                    </button>
-                    <button class="badge__icon badge__icon--blue">
-                        <BsFillPersonBadgeFill />
-                    </button>
-                    <button
-                        class="button--sm button__solid button--blue button__rounded--lg"
-                    >
-                        <span class="button__typography typography--white">Logout</span>
-                    </button>
-                </ul>
-            </nav>
-        </header>
-    )
+            <BsFillHeartFill />
+          </button>
+          <button className="badge__icon badge__icon--blue">
+            <BsFillCartFill />
+          </button>
+          <button className="badge__icon badge__icon--blue">
+            <BsFillPersonBadgeFill />
+          </button>
+          <button className="button--sm button__solid button--blue button__rounded--lg">
+            <span className="button__typography typography--white">Logout</span>
+          </button>
+        </ul>
+      </nav>
+    </header>
+  );
 }
