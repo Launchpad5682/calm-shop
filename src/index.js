4f6946b73e4dddf77b7a875243f6df4ba0ed0097
launchpad5682 <launchpad5682@gmail.com>
2022-03-17 17:28:02 +0530

    feat: Provider, and BrowserRouter added to App

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
+import { Provider } from "./context/provider-context";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
