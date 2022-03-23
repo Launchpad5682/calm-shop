addcbe7f584c605344360150cc39f4f377cd07ce
launchpad5682 <launchpad5682@gmail.com>
2022-03-23 21:00:44 +0530

    refactor: updated data-context with new state variables and imports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
-import { DataProvider } from "./context/data-provider-context";
-import { AuthProvider } from "./context/auth-provider-context";
+import { DataProvider } from "./context/data-context";
+import { AuthProvider } from "./context/auth-context";
 
 // Call make Server
 makeServer();
