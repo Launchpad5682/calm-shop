5e80eaf75a71e0084ea9d1e6b72008ff1eadd650
launchpad5682 <launchpad5682@gmail.com>
2022-03-23 21:05:21 +0530

    feat: Cart added

--- src/routes/Cart/Cart.jsx
+++ src/routes/Cart/Cart.jsx
@@ -1,5 +1,33 @@
+// import axios from "axios";
 import React from "react";
+import { CartPriceCard } from "../../components/";
+import { EmptyCart } from "../../components/";
+import { ProductCartCard } from "../../components/";
+import { useDataProvider } from "../../context/data-context";
+import { getCartTotal } from "../../utils/";
+import "./Cart.css";
 
 export function Cart() {
-  return <div>Cart</div>;
+  const { cart } = useDataProvider();
+
+  const cartTotal = getCartTotal(cart);
+
+  return (
+    <main className="cart__section padding--sm">
+      {cart.length > 0 ? (
+        <>
+          <div className="wishlist--cards">
+            {cart?.map((product) => (
+              <ProductCartCard key={product._id} product={product} />
+            ))}
+          </div>
+          <div>
+            <CartPriceCard cartTotal={cartTotal} />
+          </div>
+        </>
+      ) : (
+        <EmptyCart caption="EMPTY CART" />
+      )}
+    </main>
+  );
 }
