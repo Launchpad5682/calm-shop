1079ff33c71f00a3a4930ec6960b2bbaa18acea1
launchpad5682 <launchpad5682@gmail.com>
2022-03-23 21:05:38 +0530

    feat: Wishlist updated

--- src/routes/WishList/WishList.jsx
+++ src/routes/WishList/WishList.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { WishListCard } from "../../components/";
+import { EmptyCart } from "../../components/Cart/EmptyCart/EmptyCart";
+import { useDataProvider } from "../../context/data-context";
+import "./WishList.css";
 
 export function WishList() {
-  return <div>WishList</div>;
+  const { wishlist } = useDataProvider();
+
+  return (
+    <main className="wishlist__section">
+      {wishlist?.length > 0 ? (
+        <div className="grid-5-items">
+          {wishlist?.map((product) => (
+            <WishListCard product={product} key={product._id} />
+          ))}
+        </div>
+      ) : (
+        <EmptyCart caption="EMPTY WISHLIST" />
+      )}
+    </main>
+  );
 }
